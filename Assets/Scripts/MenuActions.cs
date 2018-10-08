0f886acde31ee4cf0663f97438e54d1ced8ed0e0
Fabricio <fabricio-gf@hotmail.com>
2018-10-08 17:19:40 -0300

    Started developing scripts

---
+++ Assets/Scripts/MenuActions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuActions : MonoBehaviour {
+
+	[Header("References")]
+	[SerializeField] private GameObject MusicManager;
+	[SerializeField] private GameObject SFXManager;
+	[SerializeField] private GameObject VoiceManager;
+	[SerializeField] private GameObject NarratorManager;
+
+	private GameObject ActivePanel;
+	private GameObject ActiveWindow;
+
+
+	public void ChangePanel(GameObject panel){
+		//deactivate current panel
+		panel.SetActive(true);
+	}
+
+	public void TriggerWindow(GameObject window){
+		//deactivate current window
+		window.SetActive(true);
+	}
+
+	public void MuteAll(bool mute){
+		
+	}
+
+	public void ChangeMusicVolume(float volume){
+
+	}
+
+	public void ChangeSFXVolume(float volume){
+		
+	}
+
+	public void ChangeVoiceVolume(float volume){
+		
+	}
+
+	public void ChangeNarratorVolume(float volume){
+		
+	}
+}
---
+++ Assets/Scripts/SelectionArrow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionArrow : MonoBehaviour {
+
+	public bool isActive;
+	public bool isSelected;
+	public bool PlayerIndex;
+	public int SelectedItem;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void MoveArrow(string direction){
+		
+	}
+}
