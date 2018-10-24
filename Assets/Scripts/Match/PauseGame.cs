d02e3e1aebb265102a230ef4825201f96748a952
Fabricio <fabricio-gf@hotmail.com>
2018-10-24 19:51:37 -0300

    Just created pause script

---
+++ Assets/Scripts/Match/PauseGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckPauseInput();
+	}
+
+	private void CheckPauseInput(){
+		
+	}
+}
