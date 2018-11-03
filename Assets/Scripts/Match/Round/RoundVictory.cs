ed038225813d18ac1b3cbd297ddec6cb819c2ff1
Fabricio <fabricio-gf@hotmail.com>
2018-11-03 16:07:56 -0300

    Reorganized scripts and added round and match victory windows

--- Assets/Scripts/Match/Round/RoundVictory.cs
+++ Assets/Scripts/Match/Round/RoundVictory.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MatchVictory : MonoBehaviour {
+public class RoundVictory : MonoBehaviour {
 
 	[SerializeField] private UnityEngine.UI.Text VictoryText;
 
 	public void UpdateVictoryText(int player){
-		VictoryText.text = "Player " + player + " wins the match!";
+		VictoryText.text = "Player " + player + " wins the round!";
 	}
 
 	public void DeactivateVictoryWindow(){
 		VictoryText.text = "";
 		gameObject.SetActive(false);
 	}
+	
 }
