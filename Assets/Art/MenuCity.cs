1561c61c6a6490680dbd42e2097f076bfbdb8b48
Unknown <will.leidan@gmail.com>
2018-11-04 22:11:09 -0200

    The Best Menu Ever

---
+++ Assets/Art/MenuCity.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCity : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void goPlay(){
+		anim.SetTrigger("play");
+	}
+
+	public void goControls(){
+		anim.SetTrigger("controls");
+	}
+
+	public void goCredits(){
+		anim.SetTrigger("credits");
+	}
+
+	public void goExit(){
+		anim.SetTrigger("exit");
+	}
+
+	public void goBack(){
+		anim.SetTrigger("back");
+	}
+
+}
--- Assets/Scripts/Player/ChangeColor.cs
+++ Assets/Scripts/Player/ChangeColor.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Anima2D;
+using UnityEngine.UI;
 
 public class ChangeColor : MonoBehaviour {
 
+    public bool ui;
 	public SpriteMeshInstance[] suit;
+    public Image[] overlays;
 	[Header("Colors")]
 	public Color yellow;
 	public Color red;
@@ -17,28 +20,55 @@ public class ChangeColor : MonoBehaviour {
 	public int color;
 
 	void OnValidate(){
-        foreach(SpriteMeshInstance s in suit){
-			if (color == 1)
-                s.color = yellow;
-            else if (color == 2)
-                s.color = red;
-            else if (color == 3)
-                s.color = blue;
-            else if (color == 4)
-                s.color = green;
+        if (ui){
+            foreach(Image s in overlays){
+                if (color == 1)
+                    s.color = yellow;
+                else if (color == 2)
+                    s.color = red;
+                else if (color == 3)
+                    s.color = blue;
+                else if (color == 4)
+                    s.color = green;
+            }
+            
+        }else{
+            foreach(SpriteMeshInstance s in suit){
+                if (color == 1)
+                    s.color = yellow;
+                else if (color == 2)
+                    s.color = red;
+                else if (color == 3)
+                    s.color = blue;
+                else if (color == 4)
+                    s.color = green;
+            }
         }
     }
 
 	public void ManualValidate(){
-		foreach(SpriteMeshInstance s in suit){
-			if (color == 1)
-                s.color = yellow;
-            else if (color == 2)
-                s.color = red;
-            else if (color == 3)
-                s.color = blue;
-            else if (color == 4)
-                s.color = green;
+		if (ui){
+            foreach(Image s in overlays){
+                if (color == 1)
+                    s.color = yellow;
+                else if (color == 2)
+                    s.color = red;
+                else if (color == 3)
+                    s.color = blue;
+                else if (color == 4)
+                    s.color = green;
+            }
+        }else{
+            foreach(SpriteMeshInstance s in suit){
+                if (color == 1)
+                    s.color = yellow;
+                else if (color == 2)
+                    s.color = red;
+                else if (color == 3)
+                    s.color = blue;
+                else if (color == 4)
+                    s.color = green;
+            }
         }
 	}
 }
