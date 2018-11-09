8d6b40b94ec0e80334a7166f68e44bc0226bc2db
Newairs <gabriel_arantes18@hotmail.com>
2018-11-08 23:01:30 -0200

    Rumble tweak

--- Assets/Scripts/Player/PlayerVibrationEvtCtlr.cs
+++ Assets/Scripts/Player/PlayerVibrationEvtCtlr.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerStateMachine))]
 public class PlayerVibrationEvtCtlr : RuntimeMonoBehaviour
 {
+    // ----------------------------------- PUBLIC TEST ----------------------------------- //
+    public AnimationCurve m_intensity = new AnimationCurve(new Keyframe(0, 1), new Keyframe(1, 0));
 
     // ------------------------------- PRIVATE ATTRIBUTES -------------------------------- //
     private PlayerInputCtlr m_input;
     private float m_delayDeath = .5f;
     private float m_delayDamage = .5f;
 
-    private float m_maxIntensityStun = 1f;
-    private float m_maxIntensityDeath = 4f;
+    private float m_maxIntensityStun = 4f;
+    private float m_maxIntensityDeath = 10f;
+    private float m_maxIntensityDamage = 7f;
 
     // ======================================================================================
     protected override void StartPhase()
@@ -30,7 +33,7 @@ public class PlayerVibrationEvtCtlr : RuntimeMonoBehaviour
     // ======================================================================================
     public void MSG_Damage()
     {
-        StartCoroutine(Vibrate(m_delayDamage, m_maxIntensityStun));
+        StartCoroutine(Vibrate(m_delayDamage, m_maxIntensityDamage));
     }
 
     // ======================================================================================
@@ -42,19 +45,15 @@ public class PlayerVibrationEvtCtlr : RuntimeMonoBehaviour
     // ======================================================================================
     private IEnumerator Vibrate(float _delay, float _maxIntesity)
     {
-        float timer = _delay;
-        float intensity = _maxIntesity;
+        float timer = 0;
 
-        while (timer > 0)
+        while (timer < _delay)
         {
-            if (timer/_delay < .8f)
-                intensity = Mathf.Lerp(0, intensity, timer / _delay);
-
-            InputMgr.VibrateController((int)m_input.m_nbPlayer, intensity, intensity);
+            InputMgr.VibrateController((int)m_input.m_nbPlayer, m_intensity.Evaluate(timer/_delay)*_maxIntesity, m_intensity.Evaluate(timer/_delay)*_maxIntesity);
 
             yield return null;
 
-            timer -= Time.deltaTime;
+            timer += Time.deltaTime;
         }
     }
 }
