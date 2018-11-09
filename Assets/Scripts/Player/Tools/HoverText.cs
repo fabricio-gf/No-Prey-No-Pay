39a6bf08e71c5ddcda572fd97701de7c8ea4584a
Fabricio <fabricio-gf@hotmail.com>
2018-11-09 00:39:29 -0200

    Added hover text

---
+++ Assets/Scripts/Player/Tools/HoverText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverText : MonoBehaviour {
+
+	public Transform PlayerTransform;
+
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = new Vector3(Mathf.Sign(PlayerTransform.localScale.x)*Mathf.Abs(transform.localScale.x),transform.localScale.y,transform.localScale.z);
+	}
+}
